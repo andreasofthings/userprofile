4fe535f8bd87defbc7f1425210afd447d3ff1f53
Andreas Neumeier <andreas@neumeier.org>
2015-03-01 11:25:04 +0100

    test homepage

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class ViewsTest(TestCase):
+    """
+    TestCase to test all exposed views for user.
+    """
+
+    def setUp(self):
+        pass
+
+    def testHome(self):
+        response = self.client.get('/user/')
+        self.assertEquals(response.status_code, 200)
