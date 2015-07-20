fac99a7d2a0c91290e6d7fd3e218c65a1a57be9b
Andreas Neumeier <andreas@neumeier.org>
2015-07-20 06:49:47 +0200

    admin.py

--- userprofile/admin.py
+++ userprofile/admin.py
@@ -1,13 +1,13 @@
 from django.contrib import admin
 from .models import Profile
 
+
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     """
     Profile admin options
     """
     list_display = (
-        'name',
         'gender',
         'age'
     )
