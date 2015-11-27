c8a9260ca8838beeba2c36eff713db151f6c472d
Andreas Neumeier <andreas@neumeier.org>
2015-11-27 20:43:36 +0100

    add pyaml

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ setup(
     install_requires=[
         'Django==1.8.5',
         'django-crispy-forms==1.2.3',
-        'python-social-auth>=0.2.2'
+        'python-social-auth>=0.2.2',
+        'pyyaml',
     ],
 )
