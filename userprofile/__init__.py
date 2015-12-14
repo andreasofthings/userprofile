f0daa0ae8c94fa3016a86a4c0e4da68650bebddb
Andreas Neumeier <andreas@neumeier.org>
2015-12-14 13:52:39 +0100

    moved CONSTANTS out of the way.

--- userprofile/__init__.py
+++ userprofile/__init__.py
@@ -1,16 +1,1 @@
-from django.utils.translation import gettext_lazy as _
-
-
-__version__ = '0.2.1'
-
-GENDER_CHOICES = (
-    ('u', _('undefined')),
-    ('M', _('Male')),
-    ('F', _('Female')),
-)
-
-LOOKFOR_CHOICES = (
-    ('a', _('any')),
-    ('M', _('Man')),
-    ('F', _('Female')),
-)
+__version__ = '0.2.2'
--- userprofile/models.py
+++ userprofile/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 
 from datetime import date
 
-from . import GENDER_CHOICES
-from . import LOOKFOR_CHOICES
+GENDER_CHOICES = (
+    ('u', _('undefined')),
+    ('M', _('Male')),
+    ('F', _('Female')),
+)
+
+LOOKFOR_CHOICES = (
+    ('a', _('any')),
+    ('M', _('Man')),
+    ('F', _('Female')),
+)
 
 
 class ProfileManager(models.Manager):
